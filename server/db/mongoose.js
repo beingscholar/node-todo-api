00eb5fbb2e8e0a0c9cb4c36f8aadaa2cf4d0b028
Mayank Agarwal <mayank3242@gmail.com>
2018-04-01 21:12:36 +0530

    Tets DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const dbName = 'todoapp';
-const Url = 'mongodb://tododb:tododb123@ds237855.mlab.com:37855/';
-//|| 'mongodb://localhost:27017/'
+const dbName = 'TodoApp';
+// const Url = 'mongodb://tododb:tododb123@ds237855.mlab.com:37855/';
+const Url = 'mongodb://localhost:27017/'
 
 mongoose.Promise = global.Promise;
 mongoose.connect(Url + dbName);
