4df917ed64c83f6ef0b18d95e3885a0358dedeae
Mayank Agarwal <mayank3242@gmail.com>
2018-04-02 02:25:26 +0530

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -88,7 +88,7 @@ app.patch('/todos/:id', (req, res) => {
     }
 
     if (_.isBoolean(body.completed) && body.completed) {
-        body.completedAt = NumberInt(new Date().getTime());
+        body.completedAt = new Date().getTime();
     } else {
         body.completed = false;
         body.completedAt = null;
