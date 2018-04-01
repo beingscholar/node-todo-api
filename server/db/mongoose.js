9fd3d6c27146d167167be8c693211061ea7a53a9
Mayank Agarwal <mayank3242@gmail.com>
2018-04-01 17:42:11 +0530

    Add test cases for GET /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
-const dbName = 'TodoApp';
-const Url = process.env.MONGOHQ_URL || 'mongodb://localhost:27017/'
+const dbName = 'todoapp';
+const Url = 'mongodb://beingscholar:Mayank@0929@ds237855.mlab.com:37855/';
+//|| 'mongodb://localhost:27017/'
 
 mongoose.Promise = global.Promise;
 mongoose.connect(Url + dbName);
 
 module.exports = { mongoose };
+
